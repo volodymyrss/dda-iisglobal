82587131d22a954c93a9b35280c8d5d3a3afd865
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-10-02 11:10:58 +0200

    set attr in class

--- iisglobal.py
+++ iisglobal.py
@@ -1,4 +1,9 @@
 from dataanalysis import analysisfactory
 
-analysisfactory.AnalysisFactory.get_by_name('ghost_bustersSpectra').cached=True
-analysisfactory.AnalysisFactory.get_by_name('ii_spectra_extract').fullbkg=True
+#analysisfactory.AnalysisFactory.get_by_name('ghost_bustersSpectra').cached=True
+
+c = analysisfactory.AnalysisFactory.get_by_name('ii_spectra_extract')
+
+c.__class__.fullbkg=True
+#c.promote()
+
